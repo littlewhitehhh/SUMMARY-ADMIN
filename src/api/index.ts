63237db9c67ead24b5174623ddf397f5f2a0dbf4
littlewhite33z <1021014610@qq.com>
2023-09-04 14:17:23 +0800

    feat: 项目开发环境搭建完成

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,145 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import md5 from 'js-md5';
-const instance = axios.create({
-  timeout: 2000,
+import { ResultData } from './types/index';
+
+// import { ResultEnum } from '@/enums/httpsEnums';
+export interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  noLoading?: boolean;
+}
+
+// const instance = axios.create({
+//   timeout: 2000,
+//   baseURL: import.meta.env.VITE_API_URL as string,
+//   withCredentials: true
+// });
+// export default instance;
+
+// export const test = () => {
+//   // console.log('test');
+
+//   instance
+//     .post('/geeker/login', {
+//       username: 'admin',
+//       password: md5('123456')
+//     })
+//     .then(res => {
+//       console.log(res);
+//     });
+// };
+
+// export const test2 = () => {
+//   // console.log('test');
+
+//   instance.get('/geeker/menu/list').then(res => {
+//     console.log(res);
+//   });
+// };
+
+const config = {
   baseURL: import.meta.env.VITE_API_URL as string,
-  withCredentials: true
-});
-export default instance;
+  timeout: 3000,
+  withCredentials: true //是否跨域？
+};
+
+// 封装一个class 更舒服一些
+
+class HttpRequest {
+  service: AxiosInstance;
+
+  constructor(config: AxiosRequestConfig) {
+    this.service = axios.create(config);
+
+    /**
+     * @description 请求拦截器
+     * 客户端发送请求 -> [请求拦截器] -> 服务器
+     * token校验（JWT）： 接受服务器返回的token，存储到pinia/vuex/本地存储当中
+     */
+    this.service.interceptors.request.use(
+      (config: CustomAxiosRequestConfig) => {
+        if (config.headers && typeof config.headers.set === 'function') {
+          config.headers.set('x-access-token', 'tttt');
+        }
+
+        return config;
+      },
+      (error: AxiosError) => {
+        return Promise.reject(error);
+      }
+    );
+
+    /**
+     * @description 响应拦截器
+     * 服务器返回信息 -> [拦截器统一处理] -> 客户端JS获取到信息
+     */
+    this.service.interceptors.response.use(
+      (response: AxiosResponse) => {
+        const { data } = response;
+
+        // 登录失效
+        // if (data.code == '404') {
+        // }
+
+        // 登录成功
+
+        return data;
+      },
+      error => {
+        Promise.reject(error);
+      }
+    );
+  }
+
+  /**
+   * @description  常用请求方法封装
+   */
+  // request<T>(config: CustomAxiosRequestConfig): Promise<T> {
+  //   return new Promise((resolve, reject) => {
+  //     this.service.request(config).then(
+  //       res => {
+  //         resolve(res.data);
+  //       },
+  //       err => {
+  //         reject(err);
+  //       }
+  //     );
+  //   });
+  // }
+  get<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+    return this.service.get(url, { params, ..._object });
+  }
+
+  post<T>(url: string, params?: object | string, _object = {}): Promise<ResultData<T>> {
+    return this.service.post(url, params, _object);
+  }
+  put<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+    return this.service.put(url, params, _object);
+  }
+  delete<T>(url: string, params?: any, _object = {}): Promise<ResultData<T>> {
+    return this.service.delete(url, { params, ..._object });
+  }
+  download(url: string, params?: object, _object = {}): Promise<BlobPart> {
+    return this.service.post(url, params, { ..._object, responseType: 'blob' });
+  }
+}
+
+export default new HttpRequest(config);
+const Request = new HttpRequest(config);
 
 export const test = () => {
+  // console.log('test');
+
+  Request.post('/geeker/login', {
+    username: 'admin',
+    password: md5('123456')
+  }).then(res => {
+    console.log(res);
+  });
+};
+
+export const test2 = () => {
   console.log('test');
 
-  instance
-    .post('/geeker/login', {
-      username: 'admin',
-      password: md5('123456')
-    })
-    .then(res => {
-      console.log(res);
-    });
+  Request.get('/geeker/menu/list').then(res => {
+    console.log(res);
+  });
 };
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 // import { test } from '@/api';
-import { test } from '../../src/api';
+import { test, test2 } from '../../src/api';
 
 const loginTest = async () => {
   let res = await test();
+  console.log(res);
+};
+const loginTest2 = async () => {
+  let res = await test2();
   console.log(res);
 };
 </script>
@@ -11,6 +15,7 @@ const loginTest = async () => {
   <div id="app">
     456
     <el-button type="success" @click="loginTest">123132</el-button>
+    <el-button type="success" @click="loginTest2">123132213</el-button>
     <div class="scssTest">scss Test</div>
     <a>test postcss autoprefixer</a>
   </div>
